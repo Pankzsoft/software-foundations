3b46e7d07196b6380cd50d81d78c686ea292fdcb
Arnaud Bailly <arnaud.oqube@gmail.com>
2020-04-03 15:30:44 +0200

    added work on Induction

--- lf/.coqdeps.d
+++ lf/.coqdeps.d
@@ -1,76 +1,76 @@
-Preface.vo Preface.glob Preface.v.beautified: Preface.v
-Preface.vio: Preface.v
-Basics.vo Basics.glob Basics.v.beautified: Basics.v
-Basics.vio: Basics.v
-Induction.vo Induction.glob Induction.v.beautified: Induction.v Basics.vo
+Preface.vo Preface.glob Preface.v.beautified Preface.required_vo: Preface.v 
+Preface.vio: Preface.v 
+Basics.vo Basics.glob Basics.v.beautified Basics.required_vo: Basics.v 
+Basics.vio: Basics.v 
+Induction.vo Induction.glob Induction.v.beautified Induction.required_vo: Induction.v Basics.vo
 Induction.vio: Induction.v Basics.vio
-Lists.vo Lists.glob Lists.v.beautified: Lists.v Induction.vo
+Lists.vo Lists.glob Lists.v.beautified Lists.required_vo: Lists.v Induction.vo
 Lists.vio: Lists.v Induction.vio
-Poly.vo Poly.glob Poly.v.beautified: Poly.v Lists.vo
+Poly.vo Poly.glob Poly.v.beautified Poly.required_vo: Poly.v Lists.vo
 Poly.vio: Poly.v Lists.vio
-Tactics.vo Tactics.glob Tactics.v.beautified: Tactics.v Poly.vo
+Tactics.vo Tactics.glob Tactics.v.beautified Tactics.required_vo: Tactics.v Poly.vo
 Tactics.vio: Tactics.v Poly.vio
-Logic.vo Logic.glob Logic.v.beautified: Logic.v Tactics.vo
+Logic.vo Logic.glob Logic.v.beautified Logic.required_vo: Logic.v Tactics.vo
 Logic.vio: Logic.v Tactics.vio
-IndProp.vo IndProp.glob IndProp.v.beautified: IndProp.v Logic.vo
+IndProp.vo IndProp.glob IndProp.v.beautified IndProp.required_vo: IndProp.v Logic.vo
 IndProp.vio: IndProp.v Logic.vio
-Maps.vo Maps.glob Maps.v.beautified: Maps.v
-Maps.vio: Maps.v
-ProofObjects.vo ProofObjects.glob ProofObjects.v.beautified: ProofObjects.v IndProp.vo
+Maps.vo Maps.glob Maps.v.beautified Maps.required_vo: Maps.v 
+Maps.vio: Maps.v 
+ProofObjects.vo ProofObjects.glob ProofObjects.v.beautified ProofObjects.required_vo: ProofObjects.v IndProp.vo
 ProofObjects.vio: ProofObjects.v IndProp.vio
-IndPrinciples.vo IndPrinciples.glob IndPrinciples.v.beautified: IndPrinciples.v ProofObjects.vo
+IndPrinciples.vo IndPrinciples.glob IndPrinciples.v.beautified IndPrinciples.required_vo: IndPrinciples.v ProofObjects.vo
 IndPrinciples.vio: IndPrinciples.v ProofObjects.vio
-Rel.vo Rel.glob Rel.v.beautified: Rel.v IndProp.vo
+Rel.vo Rel.glob Rel.v.beautified Rel.required_vo: Rel.v IndProp.vo
 Rel.vio: Rel.v IndProp.vio
-Imp.vo Imp.glob Imp.v.beautified: Imp.v Maps.vo
+Imp.vo Imp.glob Imp.v.beautified Imp.required_vo: Imp.v Maps.vo
 Imp.vio: Imp.v Maps.vio
-ImpParser.vo ImpParser.glob ImpParser.v.beautified: ImpParser.v Maps.vo Imp.vo
+ImpParser.vo ImpParser.glob ImpParser.v.beautified ImpParser.required_vo: ImpParser.v Maps.vo Imp.vo
 ImpParser.vio: ImpParser.v Maps.vio Imp.vio
-ImpCEvalFun.vo ImpCEvalFun.glob ImpCEvalFun.v.beautified: ImpCEvalFun.v Imp.vo Maps.vo
+ImpCEvalFun.vo ImpCEvalFun.glob ImpCEvalFun.v.beautified ImpCEvalFun.required_vo: ImpCEvalFun.v Imp.vo Maps.vo
 ImpCEvalFun.vio: ImpCEvalFun.v Imp.vio Maps.vio
-Extraction.vo Extraction.glob Extraction.v.beautified: Extraction.v ImpCEvalFun.vo Imp.vo ImpParser.vo Maps.vo
+Extraction.vo Extraction.glob Extraction.v.beautified Extraction.required_vo: Extraction.v ImpCEvalFun.vo Imp.vo ImpParser.vo Maps.vo
 Extraction.vio: Extraction.v ImpCEvalFun.vio Imp.vio ImpParser.vio Maps.vio
-Auto.vo Auto.glob Auto.v.beautified: Auto.v Maps.vo Imp.vo
+Auto.vo Auto.glob Auto.v.beautified Auto.required_vo: Auto.v Maps.vo Imp.vo
 Auto.vio: Auto.v Maps.vio Imp.vio
-Postscript.vo Postscript.glob Postscript.v.beautified: Postscript.v
-Postscript.vio: Postscript.v
-Bib.vo Bib.glob Bib.v.beautified: Bib.v
-Bib.vio: Bib.v
-PrefaceTest.vo PrefaceTest.glob PrefaceTest.v.beautified: PrefaceTest.v Preface.vo
+Postscript.vo Postscript.glob Postscript.v.beautified Postscript.required_vo: Postscript.v 
+Postscript.vio: Postscript.v 
+Bib.vo Bib.glob Bib.v.beautified Bib.required_vo: Bib.v 
+Bib.vio: Bib.v 
+PrefaceTest.vo PrefaceTest.glob PrefaceTest.v.beautified PrefaceTest.required_vo: PrefaceTest.v Preface.vo
 PrefaceTest.vio: PrefaceTest.v Preface.vio
-BasicsTest.vo BasicsTest.glob BasicsTest.v.beautified: BasicsTest.v Basics.vo
+BasicsTest.vo BasicsTest.glob BasicsTest.v.beautified BasicsTest.required_vo: BasicsTest.v Basics.vo
 BasicsTest.vio: BasicsTest.v Basics.vio
-InductionTest.vo InductionTest.glob InductionTest.v.beautified: InductionTest.v Induction.vo
+InductionTest.vo InductionTest.glob InductionTest.v.beautified InductionTest.required_vo: InductionTest.v Induction.vo
 InductionTest.vio: InductionTest.v Induction.vio
-ListsTest.vo ListsTest.glob ListsTest.v.beautified: ListsTest.v Lists.vo
+ListsTest.vo ListsTest.glob ListsTest.v.beautified ListsTest.required_vo: ListsTest.v Lists.vo
 ListsTest.vio: ListsTest.v Lists.vio
-PolyTest.vo PolyTest.glob PolyTest.v.beautified: PolyTest.v Poly.vo
+PolyTest.vo PolyTest.glob PolyTest.v.beautified PolyTest.required_vo: PolyTest.v Poly.vo
 PolyTest.vio: PolyTest.v Poly.vio
-TacticsTest.vo TacticsTest.glob TacticsTest.v.beautified: TacticsTest.v Tactics.vo
+TacticsTest.vo TacticsTest.glob TacticsTest.v.beautified TacticsTest.required_vo: TacticsTest.v Tactics.vo
 TacticsTest.vio: TacticsTest.v Tactics.vio
-LogicTest.vo LogicTest.glob LogicTest.v.beautified: LogicTest.v Logic.vo
+LogicTest.vo LogicTest.glob LogicTest.v.beautified LogicTest.required_vo: LogicTest.v Logic.vo
 LogicTest.vio: LogicTest.v Logic.vio
-IndPropTest.vo IndPropTest.glob IndPropTest.v.beautified: IndPropTest.v IndProp.vo
+IndPropTest.vo IndPropTest.glob IndPropTest.v.beautified IndPropTest.required_vo: IndPropTest.v IndProp.vo
 IndPropTest.vio: IndPropTest.v IndProp.vio
-MapsTest.vo MapsTest.glob MapsTest.v.beautified: MapsTest.v Maps.vo
+MapsTest.vo MapsTest.glob MapsTest.v.beautified MapsTest.required_vo: MapsTest.v Maps.vo
 MapsTest.vio: MapsTest.v Maps.vio
-ProofObjectsTest.vo ProofObjectsTest.glob ProofObjectsTest.v.beautified: ProofObjectsTest.v ProofObjects.vo
+ProofObjectsTest.vo ProofObjectsTest.glob ProofObjectsTest.v.beautified ProofObjectsTest.required_vo: ProofObjectsTest.v ProofObjects.vo
 ProofObjectsTest.vio: ProofObjectsTest.v ProofObjects.vio
-IndPrinciplesTest.vo IndPrinciplesTest.glob IndPrinciplesTest.v.beautified: IndPrinciplesTest.v IndPrinciples.vo
+IndPrinciplesTest.vo IndPrinciplesTest.glob IndPrinciplesTest.v.beautified IndPrinciplesTest.required_vo: IndPrinciplesTest.v IndPrinciples.vo
 IndPrinciplesTest.vio: IndPrinciplesTest.v IndPrinciples.vio
-RelTest.vo RelTest.glob RelTest.v.beautified: RelTest.v Rel.vo
+RelTest.vo RelTest.glob RelTest.v.beautified RelTest.required_vo: RelTest.v Rel.vo
 RelTest.vio: RelTest.v Rel.vio
-ImpTest.vo ImpTest.glob ImpTest.v.beautified: ImpTest.v Imp.vo
+ImpTest.vo ImpTest.glob ImpTest.v.beautified ImpTest.required_vo: ImpTest.v Imp.vo
 ImpTest.vio: ImpTest.v Imp.vio
-ImpParserTest.vo ImpParserTest.glob ImpParserTest.v.beautified: ImpParserTest.v ImpParser.vo
+ImpParserTest.vo ImpParserTest.glob ImpParserTest.v.beautified ImpParserTest.required_vo: ImpParserTest.v ImpParser.vo
 ImpParserTest.vio: ImpParserTest.v ImpParser.vio
-ImpCEvalFunTest.vo ImpCEvalFunTest.glob ImpCEvalFunTest.v.beautified: ImpCEvalFunTest.v ImpCEvalFun.vo
+ImpCEvalFunTest.vo ImpCEvalFunTest.glob ImpCEvalFunTest.v.beautified ImpCEvalFunTest.required_vo: ImpCEvalFunTest.v ImpCEvalFun.vo
 ImpCEvalFunTest.vio: ImpCEvalFunTest.v ImpCEvalFun.vio
-ExtractionTest.vo ExtractionTest.glob ExtractionTest.v.beautified: ExtractionTest.v Extraction.vo
+ExtractionTest.vo ExtractionTest.glob ExtractionTest.v.beautified ExtractionTest.required_vo: ExtractionTest.v Extraction.vo
 ExtractionTest.vio: ExtractionTest.v Extraction.vio
-AutoTest.vo AutoTest.glob AutoTest.v.beautified: AutoTest.v Auto.vo
+AutoTest.vo AutoTest.glob AutoTest.v.beautified AutoTest.required_vo: AutoTest.v Auto.vo
 AutoTest.vio: AutoTest.v Auto.vio
-PostscriptTest.vo PostscriptTest.glob PostscriptTest.v.beautified: PostscriptTest.v Postscript.vo
+PostscriptTest.vo PostscriptTest.glob PostscriptTest.v.beautified PostscriptTest.required_vo: PostscriptTest.v Postscript.vo
 PostscriptTest.vio: PostscriptTest.v Postscript.vio
-BibTest.vo BibTest.glob BibTest.v.beautified: BibTest.v Bib.vo
+BibTest.vo BibTest.glob BibTest.v.beautified BibTest.required_vo: BibTest.v Bib.vo
 BibTest.vio: BibTest.v Bib.vio
--- lf/README.md
+++ lf/README.md
@@ -25,3 +25,61 @@ Equivalent code in idris is in [Basics.idr](Basics.idr). Note the main differenc
 * Constructor names are capitalized in order to prevent ambiguities in name resolutions. By default, lower case identifiers are _variables_
 * Proof construction is given in the _same language_ than the type and function declarations.
 * Namespaces require qualified names to prevent ambiguities inside the namespace which is more cumbersome than in Coq
+
+# Induction
+
+Idris code in [Induction.idr](Induction.idr)
+
+Compare proof by induction in Coq
+
+```
+Theorem plus_n_O : forall n:nat, n = n + 0.
+Proof.
+  intros n. induction n as [| n' IHn'].
+  - (* n = 0 *)    reflexivity.
+  - (* n = S n' *) simpl. rewrite <- IHn'. reflexivity.  Qed.
+
+```
+
+with Idris:
+
+```
+plus_n_0 : (n:Nat) -> (n + 0 = n)
+plus_n_0 Z = Refl
+plus_n_0 (S k) =
+  let ind = plus_n_0 k
+  in rewrite ind in Refl
+```
+
+* Induction hypothesis is "just" calling the function recursively with a smaller element
+
+Proof of `minus_diag` is slightly more involved in Idris:
+
+```
+minus_diag : (n:Nat) -> {auto prf: LTE n n} -> (n - n = 0)
+minus_diag Z = Refl
+minus_diag (S k) {prf = LTESucc prf' } = minus_diag k
+```
+
+The `prf` argument is needed by `(-)` (`minus`) definition: To substract to `Nat`s one has to prove the first argument is greater than or equal to the second argument. We build a proof for the inductive case by pattern-matching on the structure of `prf` which is defined by:
+
+```
+λΠ> :doc LTE
+Data type Prelude.Nat.LTE : (n : Nat) -> (m : Nat) -> Type
+    Proofs that n is less than or equal to m
+    Arguments:
+        n : Nat  -- the smaller number
+
+        m : Nat  -- the larger number
+
+    The function is: public export
+Constructors:
+    LTEZero : LTE 0 right
+        Zero is the smallest Nat
+
+        The function is: public export
+    LTESucc : LTE left right -> LTE (S left) (S right)
+        If n <= m, then n + 1 <= m + 1
+```
+
+The `auto` qualifier instructs the typechecker to look for the proof in the environment.
